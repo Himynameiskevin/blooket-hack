95c3e0ef99bcb0c0c3e7159a960597980201a86d
Himynameiskevin <95719992+Himynameiskevin@users.noreply.github.com>
2021-12-07 15:34:36 -0800

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -8,7 +8,8 @@
 
 async function getName(authToken) {
     const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
-    const data = await response.json();
+    const data = await response.json()
+   
 
     return data.name
 };
